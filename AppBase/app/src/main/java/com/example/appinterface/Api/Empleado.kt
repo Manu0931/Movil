42ea9d6881c4560dd25017cc821b4ac2d5be209d
user.email <manuel25142808@gmail.com>
2025-10-30 21:48:30 -0500

    Primer commit - nuevo repositorio

---
+++ AppBase/app/src/main/java/com/example/appinterface/Api/Empleado.kt
@@ -0,0 +1,10 @@
+package com.example.appinterface.Api
+
+data class Empleado(
+    val nombre: String,
+    val cargo: String,
+    val correo: String,
+    val contrasena: String,
+    val fechaContratacion: String,
+    val estado: String
+)
